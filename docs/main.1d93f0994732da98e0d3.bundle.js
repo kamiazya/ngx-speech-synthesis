b8c75274ab0314f433e2ce127e0bb5ae595ffa70
kamiazya <yuki@kamiazya.tech>
2019-01-27 04:04:46 +0900

    add storybook docs for demo

---
+++ docs/main.1d93f0994732da98e0d3.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(351),p(431),n.exports=p(644)},431:function(n,o,p){"use strict";p.r(o);p(432),p(632),p(640)}},[[0,1,2]]]);
